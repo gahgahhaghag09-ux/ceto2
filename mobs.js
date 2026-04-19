455e4056c26ccaca4128bffa1e3935853ece7ba4
gahgahhaghag09-ux <gahgahhaghag09@gmail.com>
2026-04-19 22:26:31 +0300

    Update and rename mobs.js to

--- mobs.js
+++ mobs.js
@@ -1,18 +1,1 @@
-const rarityRadius = {
-  common: 1,
-  unusual: 1.1,
-  rare: 1.3,
-  epic: 1.6,
-  legendary: 3,
-  mythic: 5
-};
-export const mobTypes = {
-  babyAnt: {
-    hp: 25, dmg: 10, speed: 1, r: 10, color: "grey",
-    ai: "passive", moveTime: 60, idleTime: 60 
-  },
-  ladybug: {
-    hp: 62.5, dmg: 10, speed: 1, r: 15, color: "red", 
-    ai: "passive", moveTime: 60, idleTime: 60
-  }
-};
+
